ee4049e44be686ee39c84049c5eaeb99c4abb316
Quan <qhoang2373@gmail.com>
2024-10-29 20:59:57 -0700

    finish number 1,2,3. create Ui for zombieFighters display stats like strength, agility, price, added a button and current price

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,106 @@
-
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 
 const App = () => {
+  const [team, setTeam] = useState([])
+  const [money, setMoney] = useState(100)
+  const [zombieFighters, setZombieFighters] = useState([
+      {
+        name: 'Survivor',
+        price: 12,
+        strength: 6,
+        agility: 4,
+        img: 'https://via.placeholder.com/150/92c952',
+      },
+      {
+        name: 'Scavenger',
+        price: 10,
+        strength: 5,
+        agility: 5,
+        img: 'https://via.placeholder.com/150/771796',
+      },
+      {
+        name: 'Shadow',
+        price: 18,
+        strength: 7,
+        agility: 8,
+        img: 'https://via.placeholder.com/150/24f355',
+      },
+      {
+        name: 'Tracker',
+        price: 14,
+        strength: 7,
+        agility: 6,
+        img: 'https://via.placeholder.com/150/d32776',
+      },
+      {
+        name: 'Sharpshooter',
+        price: 20,
+        strength: 6,
+        agility: 8,
+        img: 'https://via.placeholder.com/150/1ee8a4',
+      },
+      {
+        name: 'Medic',
+        price: 15,
+        strength: 5,
+        agility: 7,
+        img: 'https://via.placeholder.com/150/66b7d2',
+      },
+      {
+        name: 'Engineer',
+        price: 16,
+        strength: 6,
+        agility: 5,
+        img: 'https://via.placeholder.com/150/56acb2',
+      },
+      {
+        name: 'Brawler',
+        price: 11,
+        strength: 8,
+        agility: 3,
+        img: 'https://via.placeholder.com/150/8985dc',
+      },
+      {
+        name: 'Infiltrator',
+        price: 17,
+        strength: 5,
+        agility: 9,
+        img: 'https://via.placeholder.com/150/392537',
+      },
+      {
+        name: 'Leader',
+        price: 22,
+        strength: 7,
+        agility: 6,
+        img: 'https://via.placeholder.com/150/602b9e',
+      },
+    ]
+  );
+
+  const handAddFighter = (fighter) => {
+    console.log(fighter)
+    
+  }
 
   return (
-    <h1>Hello world!</h1>
+      <>
+      <h1>Zombie Fighters</h1>
+      <p>Team: {team.length}</p>
+      <p>Money: ${money} </p>
+      <h2>Fighters</h2>
+      <ul>
+        {zombieFighters.map((fighter) =>
+        <li key={fighter.name}>
+        <img src={fighter.img} alt={fighter.name} />
+        <p>Team Strength: {fighter.strength} </p>
+        <p>Team Agility: {fighter.agility} </p>
+        <p>Price: {fighter.price}</p>
+        <button onClick={() => handleAddFighter(fighter)}>Add</button>
+        </li>
+      )}
+        </ul>
+      </>
   );
 }
 
